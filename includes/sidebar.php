e66530dc1225ab623cee487d266c1a4491669dc0
jplute7 <indyplute@gmail.com>
2017-03-25 17:36:33 -0500

    :8ball:
    basic test added into the sidebar, testform made

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,7 +3,7 @@
 		<p>Options:</p>
 		<ul>
 			<a href = "basicTest.php"> <li>Take our default test!</li> </a>
-			<a href = "url that i dont have"> <li>Make your own test!</li> </a>
+			<a href = "testform.php"> <li>Make your own test!</li> </a>
 			<a href = "recommendationsPage.php"> <li>Recommendations to make <br> your own test!</li> </a>
 			<a href="WebsiteFramework.php"><li>Return</li></a>
 		</ul>
---
+++ testform.php
@@ -0,0 +1,24 @@
+<?php
+include 'config.php';
+include 'includes/header.php';
+include 'includes/sidebar.php';
+?>
+
+<div class="testform">
+<form>
+
+<h1> Custom Test Creation: </h1>
+
+<p> Test Name: <input type="text">  <br>
+Question: <input style = "margin-left: 11px;" type="text"> <br><br>
+Mark Correct Answer: <br>
+<input type="checkbox" name="correct answer" value="Answer 1" > <input type = "text" ans1 = "answer 1" > <br>
+<input type="checkbox" name="correct answer" value="Answer 2"> <input type = "text" ans2 = "answer 2" > <br>
+<input type="checkbox" name="correct answer" value="Answer 3" > <input type = "text" ans3 = "answer 3" > <br>
+<input type="checkbox" name="correct answer" value="Answer 4"> <input type = "text" ans4 = "answer 4" > <br>
+
+<br></p>
+
+<input type="submit" value="Submit">
+
+</div>
