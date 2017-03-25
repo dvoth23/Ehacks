167f1584b1233c588e148bac26275b4d8c2e377f
jplute7 <indyplute@gmail.com>
2017-03-25 16:52:54 -0500

    :8ball:
    magic :8ball:

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -4,7 +4,7 @@
 		<ul>
 			<a href = "basicTest.php"> <li>Take our default test!</li> </a>
 			<a href = "url that i dont have"> <li>Make your own test!</li> </a>
-			<a href = "url that i dont have"> <li>Recommendations to make <br> your own test!</li> </a>
+			<a href = "recommendationsPage.php"> <li>Recommendations to make <br> your own test!</li> </a>
 			<a href="WebsiteFramework.php"><li>Return</li></a>
 		</ul>
 </div>
