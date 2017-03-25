c2d7d6cf01b02598c38edd5b37c40abc8ce53279
jplute7 <indyplute@gmail.com>
2017-03-25 10:34:10 -0500

    Beginning of framework and Style
    Beginning of framework and style

---
+++ WebsiteFramework.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<php>
+</php>
+	<div class="header">
+		<h1>The Stimulus Project</h1>
+	</div>
+
+	<div class="sidebar">
+		<p>Disease List:</p>
+		<ul>
+			<a href = "url that i dont have"> <li>Alzheimer's</li> </a>
+			<a href = "url that i dont have">> <li>Aphasia</li> </a>
+			<a href = "url that i dont have">> <li>Placeholder</li> </a>
+		</ul>
+	</div>
+
+	<div class="question">
+		<h1>Picture</h1>
+		<h2>Actual Question</h2>
+		<p>Question out of Total Questions</p>
+	</div>
+	
+</body>
+</html>
