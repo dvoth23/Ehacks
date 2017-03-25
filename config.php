abd1f5d8aa632388c850b4f3a5fb74767b89607d
jplute7 <indyplute@gmail.com>
2017-03-25 12:45:16 -0500

    Design-v3
    added style functionality and a css folder in the includes folder

--- config.php
+++ config.php
@@ -10,4 +10,5 @@ $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS)
 
 include 'Question.php';
 
+
 ?>
--- includes/header.php
+++ includes/header.php
@@ -3,10 +3,12 @@
 <head>
 <meta charset="ISO-8859-1">
 <title>The Stimulus Project</title>
+<link rel="stylesheet" type="text/css" href="includes/css/WebsiteStyle.css">
 </head>
 <body>
 
 
+
 <div class="header">
-<h1>The Stimulus Project</h1>
+	<h1 id="headerText">The Stimulus Project</h1>
 </div>
