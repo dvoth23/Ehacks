5b2dc90387d3a06661fb1fd8c9085a2cf6e6fe68
jplute7 <indyplute@gmail.com>
2017-03-26 10:29:22 -0500

    ok
    ok

--- report.php
+++ report.php
@@ -1,37 +1,49 @@
 <?php
-$questions = questionsToObject(getQuestionsArray() );
+include "config.php";
+
+$questions = questionsToObject(getQuestionsArray());
+
 ?>
-
-<h1 style=size:22px >RESULTS</h1>
-<table>
-
+<div class="report">
+<h1 style="font-size:22px"; >RESULTS</h1>
+<table  border:2px; border-collapse:collapse> 
+<br>
 <tr>
 
-<th> Question  </th>
-<th> Correct </th>
-<th> Incorrect </th>v
+<th style="font-size:16px"> Question       </th>
+<th style="font-size:16px"> Correct        </th>
+<th style="font-size:16px"> Incorrect      </th>
 
 </tr>
 
-<?php for($i =0; $i<count($questions);++$i)?>
-{
+<?php for($i =0; $i<count($questions);++$i){?>
+
 
 
 <tr>
 
-<td> <?php $i ?> </td>
+<td> <?php echo $i?>  </td>
 
-<?php if (isCorrect() == true)?>
-{
+<?php  if ($questions[$i] ->isCorrect() == true)
+{ ?>
 <td style=background-color:green>   </td>
 <td> </td>
-}  else 
-{<td> </td>
+<?php }
+
+else {?>
+<td> </td>
 <td style=background-color:red> </td>}
 
 </tr>
+<?php }
+
 }
 
-
+if ($_GET['sendEmail'] == true && isset($_GET['email'])) { ?>
+<tr><p>Email sent to: <?php echo $_GET['email']; ?></p></tr>
+<?php 	
+}
+?>
 
 </table>
+</div>
